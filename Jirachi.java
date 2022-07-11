d014d90fb001994167ff07e4740a576310fdd059
Stephen VanLuven <101437287+Ledagra@users.noreply.github.com>
2022-07-11 13:55:08 -0700

    Added to project

---
+++ Jirachi.java
@@ -0,0 +1,11 @@
+public class Jirachi extends Hero{
+    Jirachi() {
+        super("Jirachi");
+        this.myHP = 75;
+        this.myDamageRange = 0.20;
+        this.myAttack = 50;
+        this.mySpecialAttack = this.myTarget.getHP() / 2;
+        this.myDefense = 2;
+        this.myEvasion = 0.05;
+    }
+}
---
+++ Snorlax.java
@@ -0,0 +1,32 @@
+
+public class Snorlax extends Hero{
+    private int mySleepCounter;
+
+    Snorlax() {
+        super("Snorlax");
+        this.myHP = 200;
+        this.myDamageRange = 0.15;
+        this.myAttack = 20;
+        this.mySpecialAttack = 0;
+        this.myDefense = 10;
+        this.myEvasion = 0.05;
+        this.mySleepCounter = 0;
+    }
+
+    @Override
+    public void specialAttack() {
+        this.myHP = 200;
+        this.myBattleStatus = false;
+    }
+
+    public void sleepCounter() {
+        if(!this.myBattleStatus) {
+            if(this.mySleepCounter == 2) {
+                this.mySleepCounter = 0;
+                this.myBattleStatus = true;
+            } else {
+                this.mySleepCounter++;
+            }
+        }
+    }
+}
