4639a7bcf3addeb9670c2d130bcc6c3fc3c664ea
Stephen VanLuven <101437287+Ledagra@users.noreply.github.com>
2022-07-11 14:08:41 -0700

    small fixes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DungeonCharacter test = new Snorlax();
+        System.out.println(test.toString());
+        System.out.println(Math.random());
     }
 }
