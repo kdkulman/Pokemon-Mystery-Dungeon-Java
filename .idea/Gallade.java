5a4f1176797e272ba0052ccdece20cb657188a55
Stephen VanLuven <101437287+Ledagra@users.noreply.github.com>
2022-07-11 13:51:15 -0700

    Added to project

---
+++ .idea/Gallade.java
@@ -0,0 +1,19 @@
+public class Gallade extends Hero{
+    private final double SELF_INFLICTION = 12.5;
+
+    public Gallade() {
+        super("Gallade");
+        this.myHP = 100;
+        this.myDamageRange = 0.30;
+        this.myAttack = 20;
+        this.mySpecialAttack = this.myAttack * 1.5;
+        this.myDefense = 3;
+        this.myEvasion = 0.15;
+    }
+
+    @Override
+    public void specialAttack() {
+        this.myTarget.takeDamage(this.mySpecialAttack);
+        this.takeDamage(SELF_INFLICTION);
+    }
+}
