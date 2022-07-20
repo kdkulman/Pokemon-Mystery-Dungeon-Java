b358eb552a0adb3f84eb43c7bb78a768d7764cc2
ato99-creator <ato@uw.edu>
2022-07-20 14:23:10 -0700

    test

--- src/DungeonCharacter/DungeonCharacter.java
+++ src/DungeonCharacter/DungeonCharacter.java
@@ -1,8 +1,8 @@
-package DungeonCharacter;
+package com;
 
-public class DungeonCharacter {
-    protected String myName;
-    protected double myHP;
+public abstract class DungeonCharacter extends TileObject {
+
+    protected double myHp;
     protected double myDamageRange;
     protected double myAttack;
     protected double mySpecialAttack;
@@ -11,66 +11,39 @@ public class DungeonCharacter {
     protected DungeonCharacter myTarget;
     protected boolean myBattleStatus;
 
-    DungeonCharacter(final String THE_NAME) {
-        this.myName = THE_NAME;
-        this.myHP = 0;
-        this.myDamageRange = 0;
-        this.myAttack = 0;
-        this.mySpecialAttack = 0;
-        this.myDefense = 0;
-        this.myEvasion = 0.05;
-        this.myTarget = null;
-        this.myBattleStatus = true;
-    }
-
-    public String getName() {
-        return this.myName;
-    }
-
-    public double getHP() {
-        return this.myHP;
-    }
-
-//    public int getDamage() {
-//        return this.myDamage;
-//    }
-
-    public double getAttack() {
-        return this.myAttack;
-    }
-
-    public int getDefense() {
-        return this.myDefense;
-    }
-
-    public void setTarget(final DungeonCharacter THE_ENEMY) {
+    public void setTarget(DungeonCharacter THE_ENEMY) {
         this.myTarget = THE_ENEMY;
     }
 
-    public void attack() {
-        this.myTarget.takeDamage(this.myAttack * this.myDamageRange);
-    }
+    public void attack() {}
 
-    public void specialAttack() {
-        this.myTarget.takeDamage(this.mySpecialAttack * this.myDamageRange);
-    }
+    public void specialAttack() {}
 
-    public void takeDamage(final double THE_DAMAGE) {
-        double finalDamage = THE_DAMAGE - this.myDefense;
-        if (finalDamage < 0) {
-            finalDamage = 0;
-        }
-        if(characterIsHit()) {
-            this.myHP -= finalDamage;
-        }
+    public void takeDamage(double damage) {
+        this.myDamageRange = damage;
     }
 
     private boolean characterIsHit() {
-        if (Math.random() < this.myEvasion) {
-            return false;
-        } else {
-            return true;
-        }
+        return myBattleStatus;
     }
 
+    public String getName() {
+        return super.getName();
+    }
+
+    public double getHP() {
+        return myHp;
+    }
+
+    public int getDamage() {
+        return getDamage();
+    }
+
+    public double getAttack() {
+        return myAttack;
+    }
+
+    public int getDefense() {
+        return myDefense;
+    }
 }
