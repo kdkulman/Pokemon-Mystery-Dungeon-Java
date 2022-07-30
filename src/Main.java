e76009a524e268cb1132297ee278f440d2234a08
Kevin Kulman <kdkulman@gmail.com>
2022-07-30 00:35:23 -0700

    Created HUD

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
-import GameView.GameView;
+import GameView.*;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 
 public class Main {
+    private static final int SCALE = 3;
+    private static final int BASE_TILE_SIZE = 24;
+    private static final int TILE_SIZE = BASE_TILE_SIZE * SCALE;
+    private static final int SCREEN_TILE_WIDTH = 13;    //View will be 9 tiles x 13 tiles
+    private static final int SCREEN_TILE_HEIGHT = 9;
+    private static final int SCREEN_WIDTH = SCREEN_TILE_WIDTH * TILE_SIZE;
+    private static final int SCREEN_HEIGHT = SCREEN_TILE_HEIGHT * TILE_SIZE;
 
     public static void main(String[] args) throws IOException {
-        GameView gameView = new GameView();
+        JFrame jFrame = new JFrame();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setResizable(false);
+        jFrame.setTitle("Pokemon Mystery Dungeon Java");
+        jFrame.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        jFrame.setBackground(Color.BLACK);
+        jFrame.isDoubleBuffered();
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+
+
+        //Character Quiz
+        //CharacterQuizView characterQuizView = new CharacterQuizView(jFrame);
+
+        //Start Game
+        GameView gameView = new GameView(jFrame);
     }
 
 
