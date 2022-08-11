d579ac1a6d2301082e70f07029e9040ea5334c4e
Stephen VanLuven <101437287+Ledagra@users.noreply.github.com>
2022-08-11 13:47:48 -0700

    Attack functionality stuffs

--- src/DungeonCharacter/Enemy/Enemy.java
+++ src/DungeonCharacter/Enemy/Enemy.java
@@ -7,6 +7,7 @@ import SQL.SQLTables;
 
 public class Enemy extends DungeonCharacter {
     private final double MY_CHANCE_TO_HEAL = 0.05;
+    private final double MY_CHANCE_TO_SPECIAL = 0.10;
 
     public Enemy(final String THE_NAME) {
         super(THE_NAME);
@@ -20,6 +21,10 @@ public class Enemy extends DungeonCharacter {
             System.out.println("Enemy is healing.");
             message += this.getName() + " healed!";
             this.heal(10);
+        } else if(MY_CHANCE_TO_SPECIAL > Math.random()) {
+            System.out.println("Enemy is special attacking.");
+            message += this.getName() + " special attacked " + this.getTarget() + "!";
+            this.specialAttack();
         } else {
             System.out.println("Enemy is attacking.");
             message += this.getName() + " attacked " + this.getTarget() + "!";
