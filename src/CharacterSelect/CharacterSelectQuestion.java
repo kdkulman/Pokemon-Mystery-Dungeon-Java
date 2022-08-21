e32d058aaa7b1c75fcbecca9a88d20cb5d0220be
Stephen VanLuven <101437287+Ledagra@users.noreply.github.com>
2022-08-20 22:33:55 -0700

    JavaDoc comments

--- src/CharacterSelect/CharacterSelectQuestion.java
+++ src/CharacterSelect/CharacterSelectQuestion.java
@@ -12,6 +12,18 @@ public class CharacterSelectQuestion {
     private Points myAnswer3Points;
     private Points myAnswer4Points;
 
+    /**
+     * Constructor for CharacterSelectQuestion.
+     * @param theQuestion The question to be asked.
+     * @param theAnswer1 The first answer.
+     * @param theAnswer2 The second answer.
+     * @param theAnswer3 The third answer.
+     * @param theAnswer4 The fourth answer.
+     * @param theAnswer1Points The points for answer1.
+     * @param theAnswer2Points The points for answer2.
+     * @param theAnswer3Points The points for answer3.
+     * @param theAnswer4Points The points for answer4.
+     */
     public CharacterSelectQuestion(final String theQuestion, final String theAnswer1, final String theAnswer2,
                                    final String theAnswer3, final String theAnswer4, final Points theAnswer1Points,
                                    final Points theAnswer2Points, final Points theAnswer3Points, final Points theAnswer4Points){
@@ -32,38 +44,66 @@ public class CharacterSelectQuestion {
         SNORLAX,
         MAGIKARP
     }
+    /**
+     * Getter for myQuestion.
+     * @return myQuestion.
+     */
     public String getMyQuestion() {
         return myQuestion;
     }
-
+    /**
+     * Getter for myAnswer1.
+     * @return myAnswer1.
+     */
     public String getMyAnswer1() {
         return myAnswer1;
     }
-
+    /**
+     * Getter for myAnswer2.
+     * @return myAnswer2.
+     */
     public String getMyAnswer2() {
         return myAnswer2;
     }
-
+    /**
+     * Getter for myAnswer3.
+     * @return myAnswer3.
+     */
     public String getMyAnswer3() {
         return myAnswer3;
     }
-
+    /**
+     * Getter for myAnswer4.
+     * @return myAnswer4.
+     */
     public String getMyAnswer4() {
         return myAnswer4;
     }
-
+    /**
+     * Getter for myAnswer1Points.
+     * @return myAnswer1Points.
+     */
     public Points getMyAnswer1Points() {
         return myAnswer1Points;
     }
-
+    /**
+     * Getter for myAnswer2Points.
+     * @return myAnswer2Points.
+     */
     public Points getMyAnswer2Points() {
         return myAnswer2Points;
     }
-
+    /**
+     * Getter for myAnswer3Points.
+     * @return myAnswer3Points.
+     */
     public Points getMyAnswer3Points() {
         return myAnswer3Points;
     }
-
+    /**
+     * Getter for myAnswer4Points.
+     * @return myAnswer4Points.
+     */
     public Points getMyAnswer4Points() {
         return myAnswer4Points;
     }
